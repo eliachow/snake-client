68c50094b74699109750e57c240e7c6f3ebd4e57
eliachow <eliacchow@gmail.com>
2023-02-21 09:21:09 -0800

    eslint play.js

--- play.js
+++ play.js
@@ -2,19 +2,19 @@ const net = require("net");
 
 // establishes a connection with the game server
 const connect = function () {
-    const conn = net.createConnection({
-        host: "172.22.58.187",
-        port: 50541,
-    });
+  const conn = net.createConnection({
+    host: "172.22.58.187",
+    port: 50541,
+  });
 
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
 
-    conn.on("data", (data) => {
-        console.log("Server says: ", data);
-    })
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
 
-    return conn;
+  return conn;
 };
 
 console.log("Connecting ...");
