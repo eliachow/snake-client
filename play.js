71724d5069a2e3e6736f78e87ca1692fbac4b4ae
eliachow <eliacchow@gmail.com>
2023-02-22 08:47:08 -0800

    move setupInput to input.js and export to play.js

--- play.js
+++ play.js
@@ -1,27 +1,14 @@
-const { stdin } = require("process");
+
+// const { connect } = require("./client");
+// const { setupInput } = require("./input");
 const connect = require("./client");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
+setupInput();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
-// Create a function called handleUserInput and register it as the "data" callback handler for stdin.
-
-const handleUserInput = function (key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 
