a8281058335f193ec3945d7847a025805cc8e7f5
eliachow <eliacchow@gmail.com>
2023-02-21 19:40:41 -0800

    add conn.write to write initials on the server when it connects

--- client.js
+++ client.js
@@ -2,16 +2,25 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "172.22.58.187",
+    host: "localhost",
     port: 50541,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: ECC");
+  });
+
+
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
+
+
+
 
   return conn;
 };
