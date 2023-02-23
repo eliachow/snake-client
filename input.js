6cdb3e8ee2008e3afb984473b9b6cbbc3ceb6f4f
eliachow <eliacchow@gmail.com>
2023-02-23 08:44:42 -0800

    eslint fix input file

--- input.js
+++ input.js
@@ -8,7 +8,7 @@ let connection;
 
 // setup interface to handle user input from stdin
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -21,7 +21,7 @@ const setupInput = function (conn) {
 
 // Create a function called handleUserInput and register it as the "data" callback handler for stdin.
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
